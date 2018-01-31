a4428c18b33176defe660c44eff793b777826e6b
Cem Recai Çırak <turkmavisi@ieee.org>
2018-01-31 05:11:14 +0300

    data update
    actual pv and load datas are extracted, electricity cost data is created.

--- lp model/LP_model.py
+++ lp model/LP_model.py
@@ -1,29 +1,30 @@
 from gurobipy import *
+import pandas as pd
 
 # PARAMETERS
 
-# Constant Parameters
+# Initialize Constant Parameters
 # Note: Some of these parameters are hypotetical and will be changed with true / reasonable values. 
 dT = 0.25 # Period length in Hours (h)
 T = 96 # Number of periods
 n1, n2 = [0.9, 0.9] # Efficiencies of -/- converter and -/~ inverter as Ratio (#)
-Mg, Mgl, Mgb, Mpvl, Mpvb, Mbl = [100000, 100000, 100000, 100000, 100000, 100000] # Maximum power flow limits for all lines in MegaWatts (MW)
-Msoc = 200000 # Maximum SoC in MegaWattsHours (MWh)
-SoC0 = 0 # Starting SoC in MegaWattsHours (MWh)
+Mgl, Mgb, Mpvl, Mpvb, Mbl = [100000, 100000, 100000, 100000, 100000] # Maximum power flow limits for all lines in MegaWatts (MW)
+Msoc = 500000 # Maximum SoC in MegaWattHours (MWh)
+SoC0 = 0 # Starting SoC in MegaWattHours (MWh)
 
-# Iterable Parameters
-# Note: These parameters will be read from file.
-Ppv_List = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.35, 6.94, 25.49, 65.63, 116.35, 176.04, 252.12, 335.11, 414.09, 488.87, 532.86, 595.87, 675.18, 689.83, 695.64, 713.65, 681.54, 654.22, 620.4, 598.95, 617.41, 604.52, 613.8, 598.69, 616.03, 617.59, 543.81, 475.65, 368.28, 293.54, 211.38, 135.76, 66.08, 16.35, 0.39, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-Pl_List = [16775, 16527, 16185, 16003, 15680, 15560, 15265, 15159, 14925, 14861, 14846, 15007, 15061, 15005, 15137, 15095, 14960, 14944, 15006, 15089, 15117, 15063, 15092, 15257, 15098, 15004, 15064, 15241, 15390, 15424, 15616, 15850, 16299, 16380, 16680, 17003, 17320, 17731, 18190, 18360, 18875, 18993, 19380, 19555, 19930, 20144, 20360, 20600, 20679, 20637, 20519, 20271, 20377, 20212, 19983, 19857, 19863, 19727, 19794, 19627, 19784, 19828, 19734, 19915, 19960, 20083, 20546, 20895, 21133, 21239, 21454, 21479, 21378, 21249, 21240, 20815, 20637, 20402, 20070, 19710, 19378, 18979, 18850, 18303, 18049, 17958, 17829, 17729, 17709, 17665, 17380, 17273, 17025, 16955, 16773, 16853]
-C_List = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+# Read and Initialize Iterable Parameters
+df=pd.read_csv("actual_pvgeneration.csv", converters={"actual_pvgeneration": float}, nrows=96)
+Ppv_List = df["actual_pvgeneration"].tolist() # Daily PV generation forecast in MegaWatts (MW)
+df=pd.read_csv("actual_load.csv", converters={"actual_load": float}, nrows=96)
+Pl_List = df["actual_load"].tolist() # Daily load forecast in MegaWatts (MW)
+df=pd.read_csv("costs_mwh.csv", converters={"costs_mwh": float})
+C_List = df["costs_mwh"].tolist() # Electriciy prices in TOU Tariff per MegaWattHours in Euros (€)
 
+# LP MODEL
 
-
-# MODEL
-
-# Model Function
-# Note: This function will be called for each period
-def Minimum_Cost(Ppv, Pl, C, Initial_SoC):
+# LP Minimization Model Function
+# Note: This function will be called coninuously for each period
+def LP_Minimize_Cost(Ppv, Pl, C, Initial_SoC):
     m = Model() # Model Object: m
     
     Pg, Pgl, Pgb, Ppvl, Ppvb, Pbl = [[0 for t in range(T)], [0 for t in range(T)], [0 for t in range(T)],
@@ -32,7 +33,7 @@ def Minimum_Cost(Ppv, Pl, C, Initial_SoC):
 
     # Add Decision Variables
     for t in range(T):
-        Pg[t] = m.addVar(lb=0, ub=Mg, vtype=GRB.CONTINUOUS)
+        Pg[t] = m.addVar(lb=0, vtype=GRB.CONTINUOUS)
         Pgl[t] = m.addVar(lb=0, ub=Mgl, vtype=GRB.CONTINUOUS)
         Pgb[t] = m.addVar(lb=0, ub=Mgb, vtype=GRB.CONTINUOUS)
         Ppvl[t] = m.addVar(lb=0, ub=Mpvl, vtype=GRB.CONTINUOUS)
@@ -66,11 +67,11 @@ def Minimum_Cost(Ppv, Pl, C, Initial_SoC):
         SoC_results = m.getAttr('x', SoC)
         
         s = [(round(100*SoC_results[t]/Msoc)) for t in range(T)]
-        print("SoC(%):" + " ".join(map(str, s)))
-        print("Total_Cost = ", m.objVal)
+        print("SoC (%):" + " ".join(map(str, s)))
+        print("Total_Cost (€) = ", m.objVal)
     else:
         print("NO FEASIBLE SOLUTION!")    
     
-    return None
 
-Minimum_Cost(Ppv_List, Pl_List, C_List, SoC0)
+
+LP_Minimize_Cost(Ppv_List, Pl_List, C_List, SoC0)
--- lp model/README.md
+++ lp model/README.md
@@ -2,15 +2,16 @@
 
 
 ### Done: 
-- Linear programing model is structured and implemented as a function.
+- Linear programming model is structured and implemented as a function.
 - Actual Load and PV Generation data of 31.12.2017 are used instead of forecasted ones.
-- Mostly hypotetical parameters are used.
+- Mostly hypothetical parameters are used.
 - Total cost minimization function works for one day (96 periods).
 - gurobipy solver module is used for linear optimization.
+- Load and PV generation forecasts and Time-of-Use Tariff price parameters are read from csv file.
 
 	
 ### Further works:
-- Some of these parameters are hypotetical and will be changed with true or reasonable values. 
-- Load and PV generation forecasts and Time-of-Use Tariff price parameters will be read from csv file.
-- Model function will be called for each period
+- Some of these parameters are hypothetical and will be changed with true or reasonable values. 
+- Model function will be called continuously for each period.
+
 \
